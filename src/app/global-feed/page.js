e8630c3e9977ba0becaffb273030e2ea12f6a474
CarIsFast <chinmay1raskar@gmail.com>
2024-06-13 15:14:04 +0530

    tablet size

--- src/app/global-feed/page.js
+++ src/app/global-feed/page.js
@@ -5,10 +5,10 @@ export default function GlobalFeedPage() {
     return (
         <div className="grid grid-cols-12 overflow-x-hidden">
             {/* in this mobile screen is default and css for laptop is in md: */}
-            <div className=" col-span-12 md:col-span-8 px-4 md:px-20 overflow-hidden">
+            <div className="col-span-12 lg:col-span-8 px-4 lg:px-20 overflow-hidden">
                 <GlobalFeed />
             </div>
-            <div className="hidden md:block md:col-span-4 top-16 flex-col items-start overflow-hidden">
+            <div className="hidden lg:block lg:col-span-4 top-16 flex-col items-start overflow-hidden">
                 <PopularAccount />
             </div>
         </div>
